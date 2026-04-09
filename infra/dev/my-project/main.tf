821910392baa9deb0ad9d6e6f5ede03102ee9745
Jubril Oyetunji <cyberdev01@protonmail.com>
2026-04-09 16:39:19 +0100

    repro: generate massive plan output to exceed GitHub 65536 char comment limit

--- infra/dev/my-project/main.tf
+++ infra/dev/my-project/main.tf
@@ -7,17 +7,48 @@ terraform {
   }
 }
 
-resource "random_id" "project_id" {
-  byte_length = 8
+# 300 random_string resources at 500 chars each
+# each resource produces ~600+ chars of plan output -> ~180k chars total
+resource "random_string" "large" {
+  count   = 300
+  length  = 500
+  special = true
+  upper   = true
+  lower   = true
+  numeric = true
 }
 
-resource "random_pet" "project_name" {}
-
-resource "random_integer" "project_number" {
-  min = 1000
-  max = 9999
+# 300 random_password resources at 500 chars each - plan shows "(sensitive value)"
+# but the resource block itself adds volume
+resource "random_password" "large" {
+  count            = 300
+  length           = 500
+  special          = true
+  override_special = "!@#$%^&*()-_=+[]{}|;:,.<>?"
 }
 
-output "project_id"     { value = random_id.project_id.hex }
-output "project_name"   { value = random_pet.project_name.id }
-output "project_number" { value = random_integer.project_number.result }
+# 300 random_id resources - shows hex/b64 output in plan
+resource "random_id" "large" {
+  count       = 300
+  byte_length = 64
+}
+
+# 300 random_integer resources
+resource "random_integer" "large" {
+  count = 300
+  min   = 1
+  max   = 999999999
+}
+
+# outputs to make the plan even longer
+output "strings" {
+  value = random_string.large[*].result
+}
+
+output "ids" {
+  value = random_id.large[*].hex
+}
+
+output "integers" {
+  value = random_integer.large[*].result
+}
